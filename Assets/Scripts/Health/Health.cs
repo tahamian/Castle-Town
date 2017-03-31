50d68d211910a2c05b1bc99f6c1ffba0226bc021
Ethan Ho Man Chan <chaneh@mcmaster.ca>
2017-03-31 18:05:25 -0400

    Added Empty Scripts for classes in Scripts folder for subsystems

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Template : MonoBehaviour {
+public class Health : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
