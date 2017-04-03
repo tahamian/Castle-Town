19ac1d145692c26c98763f695495474478afbe17
Kareem Abdel Mesih <abdelk2@mcmaster.ca>
2017-04-03 19:13:18 -0400

    Upload new file

---
+++ Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger:MonoBehaviour{
+
+	private string negative;
+	private string positive;
+
+	void Start(){
+		Negative();
+		Positive();
+	}
+
+	private void Negative(){
+		negative = Events.GetNeg();
+		Debug.Log(negative);
+		Invoke("Negative", Random.Range(5, 10));
+	}
+
+	private void Positive(){
+		positive = Events.GetPos();
+		Debug.Log(positive);
+		Invoke("Positive", Random.Range(10, 20));
+	}
+}
